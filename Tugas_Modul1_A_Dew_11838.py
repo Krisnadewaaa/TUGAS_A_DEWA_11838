74124f3491d03f6be8055ab9d6813c3dd2d8c63c
Krisnadewaaa <220711838@students.uajy.ac.id>
2024-09-12 14:37:36 +0700

    Tugas_Modul1_A_Dew_11838.py

--- Tugas_Modul1_A_Dew_11838.py
+++ Tugas_Modul1_A_Dew_11838.py
@@ -10,9 +10,9 @@ if os.path.exists(model_path):
         with open(model_path, 'rb') as f:
             loaded_model = pickle.load(f)
         rf_model = loaded_model[0]
-
+    
         st.title("Prediksi Diabetes")
-
+    
         st.write("Aplikasi ini digunakan untuk membantu memprediksi penyakit diabetes pada seseorang")
         pregnancies = st.slider("Pregnancies", min_value=0, max_value=17, step=1)
         glucose = st.slider("Glucose (mg/dL)", min_value=0.0, max_value=199.0, step=0.1)
@@ -22,17 +22,17 @@ if os.path.exists(model_path):
         bmi = st.slider("BMI", min_value=0.0, max_value=67.1, step=0.1)
         diabetesPedigreeFunction = st.slider("Diabetes Pedigree Function", min_value=0.07, max_value=2.42, step=0.1)
         age = st.slider("Age", min_value=21, max_value=81, step=1)
-
+    
         input_data = [[pregnancies, glucose, bloodPressure, skinThickness, insulin, bmi,
-                        diabetesPedigreeFunction, age]]
-
+                      diabetesPedigreeFunction, age]]
+    
         if st.button("Prediksi!"):
             rf_model_prediction = rf_model_predict(input_data)
             outcome_names = {0: 'Tidak Diabetes', 1: 'Diabetes'}
             st.write(f"Orang tersebut diprediksi **{outcome_names[rf_model_prediction[0]]}** oleh RF")
 
-        except Exception as e:
-            st.error("Terjadi Kesalahan: {e}")
+    except Exception as e:
+        st.error("Terjadi Kesalahan: {e}")
 
-    else:
-        print("File 'rf_diabetes_model.pkl' tidak ditemukan di direktori")
+else:
+    print("File 'rf_diabetes_model.pkl' tidak ditemukan di direktori")
